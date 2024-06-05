9c0d980da24b59375d7754388a12234b407676fe
Klaas tom Dieck <klaas.tomdieck@gmail.com>
2024-06-05 15:54:10 +0200

    even more debug

--- 1_Parallelization/HashJoin.h
+++ 1_Parallelization/HashJoin.h
@@ -168,6 +168,7 @@ std::vector<ResultRelation> performCacheSizedThreadedHashJoin(const std::vector<
     if(HASHMAP_SIZE > rightRelation.size() / numThreads) {
         std::cout << "rightRelation.size() / numThreads == 0 is " << (rightRelation.size() / numThreads == 0) << std::endl;
         HASHMAP_SIZE = (rightRelation.size() / numThreads == 0) ? 1 : rightRelation.size() / numThreads + 1;
+        std::cout << "Hash map size changed!" << std::endl;
     }
     std::cout << "Hash map size is now: " << HASHMAP_SIZE << std::endl;
     std::vector<ResultRelation> results;
