db3bd4d0a7fbfac057f2336c912b111f41f7d72f
klaas <klaas.tomdieck@gmail.com>
2024-06-14 00:31:16 +0200

    Hard coded generated_variables.h into CMakeLists.txt

--- Util/include/generated_variables.h
+++ Util/include/generated_variables.h
@@ -1,4 +1,4 @@
 #pragma once
 constexpr const size_t L1_CACHE_SIZE=32768;
-constexpr const size_t L2_CACHE_SIZE=262144;
-constexpr const size_t L3_CACHE_SIZE=16777216;
+constexpr const size_t L2_CACHE_SIZE=524288;
+constexpr const size_t L3_CACHE_SIZE=33554432;
