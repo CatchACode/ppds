c616dfeb46b7032879d8c59cad89f7158ee24999
jkorus <jkorus90@gmail.com>
2024-06-17 19:11:46 +0200

    Trie.h befüllt, da letztens nicht geklappt

--- 4_Strings/Join.cpp
+++ 4_Strings/Join.cpp
@@ -14,6 +14,7 @@
 
 #include "TimerUtil.hpp"
 #include "JoinUtils.hpp"
+#include "trie.h"
 #include <unordered_map>
 #include <iostream>
 #include <gtest/gtest.h>
@@ -22,7 +23,21 @@
 std::vector<ResultRelation> performJoin(const std::vector<CastRelation>& castRelation, const std::vector<TitleRelation>& titleRelation, int numThreads) {
     omp_set_num_threads(numThreads);
     std::vector<ResultRelation> resultTuples;
+    //------------------------- Trie Test ----------------------------------
+    vector<string> strs = {"apple", "banana", "orange", "apricot", "app"};
+    for (int i = 0; i < strs.size(); i++) {
+        cout << "Der Eintrag mit Index " << i << " lautet: " << strs[i] << endl;
+    }
+    TrieNode *root=new TrieNode();
+    for (int i = 0; i < strs.size(); i++) {
+        insert(root,strs[i]);
+    }
+    string wordToSearch = "app";
+    bool found = search(root, wordToSearch);
+    cout << "Das Wort '" << wordToSearch << "' ist im Präfixbaum enthalten: " << (found ? "Ja" : "Nein") << endl;
 
+    printTrie(root);
+    //---------------------------------------------------------------------------------------
     // TODO: Implement a join on the strings cast.note and title.title
     // The benchmark will join on increasing string sizes: cast.note% LIKE title.title
 
--- 4_Strings/trie.h
+++ 4_Strings/trie.h
@@ -5,4 +5,62 @@
 #ifndef PPDS_TRIE_H
 #define PPDS_TRIE_H
 
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<map>
+
+using namespace std;
+struct TrieNode
+{
+    map<char,TrieNode*> children;
+    bool endofword;
+    TrieNode()
+    {
+        endofword=false;
+    }
+};
+void insert(TrieNode *root,string word)
+{
+    TrieNode *current=root;
+    for(int i=0;i<word.size();i++)
+    {
+        char ch=word[i];
+        TrieNode *node=current->children[ch];
+        if(!node)
+        {
+            node = new TrieNode();
+            current->children[word[i]]=node;
+        }
+        current=node;
+    }
+    current->endofword=true;
+}
+bool search(TrieNode *root,string word)
+{
+    TrieNode *current=root;
+    for(int i=0;i<word.size();i++)
+    {
+        char ch=word[i];
+        TrieNode *node=current->children[ch];
+        if(!node)
+            return false;
+        current=node;
+    }
+    return current->endofword;
+}
+
+void printTrie(TrieNode* root, string prefix = "") {
+    if (root->endofword) {
+        cout << prefix << endl;
+    }
+    for (auto it: root->children) {
+        printTrie(it.second, prefix + it.first);
+    }
+}
+
+
 #endif //PPDS_TRIE_H
