970212c3e82372e6eb4bfcb923130eab423ed86a
klaas <klaas.tomdieck@gmail.com>
2024-06-26 14:02:30 +0200

    Added caches sizes to git

---
+++ 3_Partitioning/generated_variables.h
@@ -0,0 +1,4 @@
+// Now constantly defined -> no script
+constexpr const size_t L1_CACHE_SIZE=256*1024/8;
+constexpr const size_t L2_CACHE_SIZE=2*1024*1024/8;
+constexpr const size_t L3_CACHE_SIZE=16*1024*1024;
