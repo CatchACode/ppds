efaf29a8c39e7b503564c1c0f8e2ac8fa441d11a
Klaas tom Dieck <klaas.tomdieck@gmail.com>
2024-07-01 13:44:23 +0200

    Seems to correctly partition now

--- 3_Partitioning/TestPartitioning.cpp
+++ 3_Partitioning/TestPartitioning.cpp
@@ -12,27 +12,27 @@
 class TestPartitioning : public ::testing::Test {};
 
 TEST_F(TestPartitioning, CastPartitioning) {
-    const auto castRelation = load<CastRelation>(DATA_DIRECTORY + std::string("cast_info_zipfian1gb.csv"));
-
+    const auto castRelation = load<CastRelation>(DATA_DIRECTORY + std::string("cast_info_zipfian1kb.csv"));
+    Timer timer("Radix");
+    timer.start();
     auto result = radixCastPartition(castRelation, 8);
-
-    for(const auto& record: result) {
-        std::cout << std::bitset<32>(record.castInfoId) << std::endl;
+    timer.pause();
+    std::cout << "Timer: " << printString(timer) << "ms" << std::endl;
+    for(const auto& record: result.second) {
+        std::cout << std::bitset<32>(record->movieId) << std::endl;
     }
     std::cout << "Finished" << std::endl;
 }
 
 TEST_F(TestPartitioning, TitlePartitioning) {
-    const auto titleRelation = loadTitleRelation(DATA_DIRECTORY + std::string("title_info_uniform1gb.csv"));
+    const auto titleRelation = loadTitleRelation(DATA_DIRECTORY + std::string("title_info_uniform1kb.csv"));
     Timer timer("Radix Title Partitioning");
     timer.start();
     auto result = radixTitlePartition(titleRelation, 8);
     timer.pause();
     std::cout << "Time: " << printString(timer)<< "ms" << std::endl;
-    /*
-    for(const auto& record: result) {
-        std::cout << std::bitset<32>(record.titleId) << std::endl;
+    for(const auto record: result.second) {
+        std::cout << std::bitset<32>(record->titleId) << std::endl;
     }
-     */
     std::cout << "Finished" << std::endl;
 }
